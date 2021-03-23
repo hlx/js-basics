3cddf4ff2b68da312b028fe809826570fb1ec1ea
Greg@Home <g.wolejko@gmail.com>
2021-03-23 14:57:31 +0100

    Console commands

---
+++ scripts/console.js
@@ -0,0 +1,21 @@
+//logging to console
+
+console.log("Hello");
+console.log(123);
+console.log([1,3,5,7,9]);
+console.log({a:1,b:34,d:567});
+
+console.table({a:1,b:34,d:567});
+
+var msg = "O hai";
+console.log(msg);
+
+console.error("this is an error");
+console.warn("this is a warning");
+
+console.time("timer_test");     //starts timer
+console.timeLog("timer_test");  //outputs current time reading
+console.timeEnd("timer_test");  //ends timer
+
+//console.clear(); //clears console
+
