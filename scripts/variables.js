9148902576f53586b6b64126e6bf50e85e532b74
Greg@Home <g.wolejko@gmail.com>
2021-03-23 15:49:44 +0100

    Variables, let, const info

--- scripts/variables.js
+++ scripts/variables.js
@@ -1,5 +1,23 @@
-let person = "John Doe";
+let entity = "John Doe";
 
-console.log(person);
-person = "Ann Smith";
-console.log(person);
+console.log(entity);
+entity = "Ann Smith";
+console.log(entity);
+
+//const
+const message = "hello from const";
+// message = "boop"; //cant reassign to const
+console.log(message);
+
+//properties inside const_object can still be modified
+const const_object = {
+    name: "John",
+    age : 30
+}
+const_object.name = "Sarah";
+
+const numbers = [1,2,3,4,5];
+numbers.push(6);
+
+console.log(const_object);
+console.log(numbers);
