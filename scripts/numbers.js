c0d38f08a697ec371c66dd18055095a8e39ead1e
Greg@Home <g.wolejko@gmail.com>
2021-03-24 16:25:36 +0100

    Number Operations and Math object

---
+++ scripts/numbers.js
@@ -0,0 +1,16 @@
+const num1 = 100;
+const num2 = 60;
+let val;
+
+val = num1 + num2;
+val = num1 - num2;
+val = num1 / num2;
+val = num1 * num2;
+val = num1 % num2;
+
+val = Math.PI;
+val = Math.round(Math.PI);
+val = Math.ceil(Math.PI);
+val = Math.floor(Math.random() * 20 + 1); // random number 1-20
+
+console.log(val);
