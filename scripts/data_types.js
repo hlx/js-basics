48347ca1f74ad6a0848ce8b3866b49feaf156e6f
Greg@Home <g.wolejko@gmail.com>
2021-03-24 16:03:49 +0100

    Data types and conversions and coersions

--- scripts/data_types.js
+++ scripts/data_types.js
@@ -22,3 +22,34 @@ const today = new Date();
 
 //TYPEOF
 console.log(typeof sym +" "+typeof obj);
+
+//TYPE CONVERSIONS
+let val;
+
+//number
+val = 5;
+//number to string
+val = String(5);
+//bool to string
+val = String(true);
+// array to string
+val = String([1,2,3]);
+// same can be done with .toString()
+val = ([1,2,3,4]).toString();
+// string to number
+val = Number("5");
+// parsing int
+val = parseInt('100.32');
+// parsing float
+val = parseFloat('101.34');
+
+console.log(val);
+console.log(typeof val);
+console.log(val.length);
+
+
+//TYPE COERSION
+const val1 = "5";
+const val2 = 6;
+const sum = val1 + val2;
+console.log(sum +" "+ typeof sum);
