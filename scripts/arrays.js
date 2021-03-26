ad0cdbb33f60d2537b17bbfbc172613ca6b75fdd
Greg@Home <g.wolejko@gmail.com>
2021-03-26 16:19:57 +0100

    Array operations

--- scripts/arrays.js
+++ scripts/arrays.js
@@ -1,3 +1,31 @@
-const numbers = [1, 3, 5, 7, 9];
+const numbers = [1, 3, 5, 7, 9, 100, 33, 11];
+const numbers2 = new Array(1, 3, 5, 7, 9);
+
+let val;
+val = numbers.length;
+val = numbers.indexOf(5);
+
+numbers.push(666);
+numbers.unshift(-13);
+numbers.pop();
+numbers.shift();
+
+/*
+ .splice
+ .reverse
+ .concat
+ .sort
+*/
+
+
+numbers.sort(); //sorts like strings
+numbers.sort((x,y)=> x-y); // sorts like numbers, y-x sorts in reverse
+
+//find
+ function over10(num) {
+     return num > 10;
+ }
+ val = numbers.find(over10);
 
 console.log(numbers);
+console.log(val);
