933f7f727ac759ea798ec45a79452c12d0e2ed5e
Greg@Home <g.wolejko@gmail.com>
2021-03-27 15:33:30 +0100

    window object

---
+++ scripts/window.js
@@ -0,0 +1,24 @@
+// prompt and confirm
+// const input = prompt("input some stuff");
+// const areYouSure = confirm("Are you sure");
+// alert(`${input}   ${areYouSure}`);
+
+let val;
+val = window.innerHeight;
+
+//location object
+val = window.location;
+
+/*
+ window.location.href
+ window.location.search
+ window.location.reload();
+
+ window.hisotry.go(-number)
+ window.history.length
+
+ window.navigator
+*/
+
+
+console.log(val);
