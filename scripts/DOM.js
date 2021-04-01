1457e01f4cffd62c6301a1aae83624a497e0b81a
Greg@Home <g.wolejko@gmail.com>
2021-04-01 17:20:07 +0200

    DOM operations

---
+++ scripts/DOM.js
@@ -0,0 +1,74 @@
+/* 
+DOCUMENT OBJECT
+html element collection is not an array
+val elements = <get elements>
+val elementsArray = Array.from(elements);
+
+*/
+
+// document.getElementById()
+
+const element = document.getElementById("test");
+
+//change styling
+element.style.border = "1px solid green";
+element.style.width = "30rem";
+
+document.getElementById("test-par").textContent = "JS is happening here."; //returns full text
+document.getElementById("test-par").innerText = "JS is happening here as well."; // returns visible text, perf heavy, needs display context
+
+// querry selector
+document.querySelector(".listItem:last-child").style.color = "red";
+document.querySelector(".listItem:nth-child(2)").style.color = "pink";
+
+// .getElementsByClassName
+const items = document.getElementsByClassName("listItem");
+
+// .getElementsByTagName("li")
+
+/* TRAVERSING
+    .childNodes //node list
+    .childNodes[x].nodeType
+    .children //html collection
+    .firstChild
+    .firstElementChild
+    .lastChild
+    .lastElementChild
+    .childElementCount
+
+    .parentNode
+    .parentElement
+
+    .nextSibling
+    .nextElementSibling
+*/
+
+// creating elements
+const li = document.createElement("li");
+li.className = "listItem js-added";
+li.setAttribute("title", "new title");
+li.appendChild(document.createTextNode("New item added!"));
+document.querySelector("ul.list").appendChild(li);
+
+// replace elements
+const newHeading = document.createElement("h2");
+newHeading.innerText = "A new heading!!!";
+
+const oldHeading = document.getElementsByTagName("h5")[0];
+const headingParent = oldHeading.parentNode;
+headingParent.replaceChild(newHeading, oldHeading);
+
+/*  remove elements
+    element.remove();
+    parent.removeChild(actual element);
+*/
+
+newHeading.classList.add("new-class");
+
+if (newHeading.hasAttribute("class")) {
+    console.log("class added succesfully");
+} else {
+    console.log("class not added");
+}
+
+console.log(oldHeading);
