8ac652973445090b749157681db55d97f3c0d68c
Greg@Home <g.wolejko@gmail.com>
2021-04-02 11:56:20 +0200

    local storage + JSON formatting

---
+++ scripts/storage.js
@@ -0,0 +1,14 @@
+localStorage.setItem("name", "John");
+localStorage.setItem("age", "30");
+
+const name = localStorage.getItem("name");
+console.log(name);
+
+localStorage.removeItem("name");
+localStorage.clear();
+
+/*
+    JSON.parse();
+    JSON.stringify();
+
+*/
