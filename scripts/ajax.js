d2f6f1cff12ae37c0c921dfc47ee24f2dcc121db
Greg@Home <g.wolejko@gmail.com>
2021-04-08 11:03:53 +0200

    easyHTTP implementation

---
+++ scripts/ajax.js
@@ -0,0 +1,56 @@
+const http = new easyHTTP();
+
+// http.get(
+//     "https://jsonplaceholder.typicode.com/posts",
+//     function (err, response) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(response);
+//         }
+//     }
+// );
+
+const data = {
+    id: 122,
+    title: "foo",
+    body: "bar",
+    userId: 133,
+};
+
+http.post(
+    "https://jsonplaceholder.typicode.com/posts",
+    data,
+    function (err, response) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(response);
+        }
+    }
+);
+
+// http.put(
+//     "https://jsonplaceholder.typicode.com/posts/1",
+//     data,
+//     function (err, response) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(response);
+//         }
+//     }
+// );
+
+// http.delete(
+//     "https://jsonplaceholder.typicode.com/posts/1",
+//     function (err, response) {
+//         if (err) {
+//             console.log(err);
+//         } else {
+//             console.log(response);
+//         }
+//     }
+// );
+
+
