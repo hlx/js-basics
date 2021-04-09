48b501d6de33b480abe15604a7434331dbdd534c
Greg@Home <g.wolejko@gmail.com>
2021-04-09 18:09:08 +0200

    arrow function returning object

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,35 +1,40 @@
 // function declaration
 function greet(firstName = "John", lastName = "Smith") {
-  return `Hello ${firstName} ${lastName}`;
+    return `Hello ${firstName} ${lastName}`;
 }
 console.log(greet("Tiberius"));
 
 // function expressions
 const square = function (x) {
-  return x * x;
+    return x * x;
 };
 console.log(square(4));
 
 // immediately invokable function expressions - IIFEs
 (function (str) {
-  console.log("IIFE " + str);
+    console.log("IIFE " + str);
 })("blurpent");
 
 //property methods
 
 const todo = {
-  add: function () {
-    console.log("Add todo");
-  },
-  edit: function (id) {
-    console.log(`Edit todo ${id}`);
-  },
+    add: function () {
+        console.log("Add todo");
+    },
+    edit: function (id) {
+        console.log(`Edit todo ${id}`);
+    },
 };
 
 todo.delete = function () {
-  console.log("Delete todo");
+    console.log("Delete todo");
 };
 
 todo.add();
 todo.edit(666);
 todo.delete();
+
+//arrow functions
+
+//returning object
+const message = () => ({ msg: "hello" });
