dbc3d5231e47cbac314b35592a321368140aacf8
Greg@Home <g.wolejko@gmail.com>
2021-04-10 16:27:12 +0200

    advanced redex

--- scripts/regEx.js
+++ scripts/regEx.js
@@ -35,7 +35,31 @@ re = /h*o/i; //matches any character
 re = /gre?a?y/i; //matches optional character
 re = /gre?a?y\?/i; //escape character
 
-const str = "Gray?";
+//character sets
+re = /gr[ae]y/i; //must be a or e
+re = /[^G]ray/; //anything exept "G"
+re = /[A-Z]ray/; //any uppercase
+re = /[A-Za-z]ray/; //any case
+
+//quantifiers
+re = /Hel{2}o/i; //must occur exactly {m} times
+re = /Hel{2,4}o/i;
+re = /Hel{2,}o/i;
+
+//shorthand classes
+re = /\w/; //word character
+re = /\W/; //non word character
+re = /\d/; //digit character
+re = /\D/; //non digit character
+re = /\s/; //whitespace character
+re = /\S/; //non space character
+re = /Hell\b/i;
+
+//assertions
+re = /x(?=y)/; //match x only if followed by y
+re = /x(?!y)/; //match x only if NOT followed by y
+
+const str = "Hellox Hellxy";
 const result = re.exec(str);
 console.log(result);
 
