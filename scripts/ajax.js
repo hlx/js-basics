03c9b66894b64fc891e40c4a30c15bcb5be350ae
Greg@Home <g.wolejko@gmail.com>
2021-04-10 11:36:35 +0200

    easyHTTP with async and await with fetch

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -53,26 +53,26 @@
 //     }
 // );
 
-// const data = {
-//     name: "John Doe",
-//     username: "johnny",
-//     email: "jd@aol.com",
-// };
+const data = {
+    name: "John Doe",
+    username: "johnny",
+    email: "jd@aol.com",
+};
 
-// const http = new EasyHTTP();
+const http = new EasyHTTP();
 
-// http.get("https://jsonplaceholder.typicode.com/users")
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+http.get("https://jsonplaceholder.typicode.com/users")
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 
-// http.post("https://jsonplaceholder.typicode.com/users",data)
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+http.post("https://jsonplaceholder.typicode.com/users",data)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
 
 // http.put("https://jsonplaceholder.typicode.com/users/2", data)
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err));
 
-// http.delete("https://jsonplaceholder.typicode.com/users/2")
-//     .then(data => console.log(data))
-//     .catch(err => console.log(err));
+http.delete("https://jsonplaceholder.typicode.com/users/2")
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
