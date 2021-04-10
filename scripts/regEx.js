3fe5f358c33a8a27013ebb3783725e0c0e6059e4
Greg@Home <g.wolejko@gmail.com>
2021-04-10 12:00:48 +0200

    regex test function

--- scripts/regEx.js
+++ scripts/regEx.js
@@ -1,23 +1,40 @@
+// let re;
+// re = /hello/i;
+
+// console.log(re);
+// console.log(re.source);
+
+// //exec()
+// let res = re.exec("hello world");
+
+// //test()
+// res = re.test("Hello");
+
+// //match()
+// const str = "Hello there";
+// res = str.match(re);
+
+// //search()
+// res = str.search(re);
+// console.log(res);
+
+// //replace
+// const newStr = str.replace(re, "Hi");
+// console.log(newStr);
+
 let re;
-re = /hello/i;
+re = /hello/;
 
-console.log(re);
-console.log(re.source);
-
-//exec()
-let result = re.exec("hello world");
-
-//test()
-result = re.test("Hello");
-
-//match()
-const str = "Hello there";
-result = str.match(re);
-
-//search()
-result = str.search(re);
+const str = "Hello world";
+const result = re.exec(str);
 console.log(result);
 
-//replace
-const newStr = str.replace(re, "Hi");
-console.log(newStr);
+function reTest(re, str) {
+    if (re.test(str)) {
+        console.log(`${str} matches ${re.source}`);
+    } else {
+        console.log(`${str} does NOT match ${re.source}`);
+    }
+}
+
+reTest(re, str);
