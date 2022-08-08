dbd61ada0dd9ad6803da9c21e12b5c4f89295096
Greg@Home <g.wolejko@gmail.com>
2022-08-08 22:33:13 +0200

    random change

--- scripts/window.js
+++ scripts/window.js
@@ -10,14 +10,14 @@ val = window.innerHeight;
 val = window.location;
 
 /*
- window.location.href
- window.location.search
- window.location.reload();
+    window.location.href
+    window.location.search
+    window.location.reload();
 
- window.hisotry.go(-number)
- window.history.length
+    window.hisotry.go(-number)
+    window.history.length
 
- window.navigator
+    window.navigator
 */
 
 console.log(val);
